9bb3640812dd582081f0e434dd95b759ef1665c8
pepost <pepost@gmail.com>
2015-02-02 11:29:12 +0200

    Initial commit

---
+++ DemoWebDebToolApp/Scripts/home.js
@@ -0,0 +1,22 @@
+﻿
+$().ready(function () {
+
+$('.btn-ajax').on('click', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    var request = $.ajax({
+        url: "Home/Details",
+        type: "POST",
+        data: { id: 'test' },
+        dataType: "html"
+    });
+
+    request.done(function (msg) {
+        $("#resultArea").html(msg);
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
+});
+})
